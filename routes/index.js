436510576f4bc2b4543612a31be3cf2e0349d7e6
David Chick <dchick@mac.com>
2025-05-30 12:43:21 -0700

    configuring deployment

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
-const { Router } = require("express");
-const router = Router();
+const express = require("express");
+// const { Router } = require("express");
+const router = express.Router();
 
 router.use("/auth", require('./auth'));
 router.use("/stores", require('./stores'));
 router.use("/items", require('./items'));
 router.use("/prices", require('./prices'));
+
+router.use(express.static(__dirname + '/../frontend/dist'));
 
 module.exports = router;
