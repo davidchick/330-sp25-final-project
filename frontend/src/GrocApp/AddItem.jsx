f91b44d69e7de5fb371b3647cbb945617e03738e
David Chick <dchick@mac.com>
2025-06-22 12:35:24 -0700

    typo

--- frontend/src/GrocApp/AddItem.jsx
+++ frontend/src/GrocApp/AddItem.jsx
@@ -6,7 +6,7 @@ function AddItem({ onItemAdded }) {
     const { authToken } = useContext(AuthToken);
 
     let API_URL;
-    import.meta.env.VITE_API_URL ? API_URL = `https://${import.meta.env.VITE_API_URL}` : API_URL = 'http://localhost:3000';
+    import.meta.env.VITE_API_URL ? API_URL = `http://${import.meta.env.VITE_API_URL}` : API_URL = 'http://localhost:3000';
 
     const [formData, setFormData] = useState({
         name: '',
